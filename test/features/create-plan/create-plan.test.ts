8478684292c40f866cea5cef5d8cf34eb52bc819
Jordi Colas Retana <jordi.colas@goin.app>
2022-02-12 18:12:29 +0100

    Minor changes

--- test/features/create-plan/create-plan.test.ts
+++ test/features/create-plan/create-plan.test.ts
@@ -4,36 +4,44 @@ import { User } from "../../../src/core/shared/domain/user";
 import { PlanCreator } from "../../../src/features/create-plan/application/plan-creator";
 import { PlanCreationData } from "../../../src/features/create-plan/domain/plan-creation-data";
 
-describe("Plan creation", () => {
-  test("A plan can be created", async done => {
-    const currentUser: User = {
-      id: `${new Date().valueOf()}`,
-      name: { firstName: "Andre", lastName: "Benelli" },
-    };
+describe("Unit tests", () => {
+  describe("PlanCreator", () => {
+    test("A plan can be created", async done => {
+      const currentUser: User = {
+        id: `${new Date().valueOf()}`,
+        name: { firstName: "Andre", lastName: "Benelli" },
+      };
 
-    const planTime = new Date().valueOf();
-    const newPlan: PlanCreationData = {
-      owner: currentUser,
-      title: "A boring walk",
-      location: "Barcelona",
-      time: planTime,
-      privacy: Privacy.PUBLIC,
-      category: Category.WALK,
-    };
+      const planTime = new Date().valueOf();
+      const newPlan: PlanCreationData = {
+        owner: currentUser,
+        title: "A boring walk",
+        location: "Barcelona",
+        time: planTime,
+        privacy: Privacy.PUBLIC,
+        category: Category.WALK,
+      };
 
-    const planCreator = containerDI.resolve(PlanCreator);
-    const createdPlan = await planCreator.create(currentUser, newPlan);
+      const planCreator = containerDI.resolve(PlanCreator);
+      const createdPlan = await planCreator.create(currentUser, newPlan);
 
-    expect(createdPlan.id).toBeDefined();
-    expect(createdPlan.owner.id).toBe(currentUser.id);
-    expect(createdPlan.title).toBe(newPlan.title);
-    expect(createdPlan.privacy).toBe(newPlan.privacy);
-    expect(createdPlan.category).toBe(newPlan.category);
-    expect(createdPlan.location).toBe(newPlan.location);
-    expect(createdPlan.time).toBe(planTime);
-    expect(createdPlan.chat).toBeDefined();
-    expect(createdPlan.atendees).toBeDefined();
+      expect(createdPlan.id).toBeDefined();
+      expect(createdPlan.owner.id).toBe(currentUser.id);
+      expect(createdPlan.title).toBe(newPlan.title);
+      expect(createdPlan.privacy).toBe(newPlan.privacy);
+      expect(createdPlan.category).toBe(newPlan.category);
+      expect(createdPlan.location).toBe(newPlan.location);
+      expect(createdPlan.time).toBe(planTime);
+      expect(createdPlan.chat).toBeDefined();
+      expect(createdPlan.atendees).toBeDefined();
 
-    done();
-  }, 240000);
+      done();
+    }, 240000);
+  });
+});
+
+describe("Acceptance test", () => {
+  test("pepe", done => {
+    expect(true).not.toBe(false);
+  });
 });
--- test/features/find-plan/find-plan.test.ts
+++ test/features/find-plan/find-plan.test.ts
@@ -2,16 +2,18 @@ import { containerDI } from "../../../src/core/dependency-injection/container";
 import { Category } from "../../../src/core/shared/domain/plan";
 import { PlanFinder } from "../../../src/features/find-plan/application/plan-finder";
 
-describe("Plan search", () => {
-  test("A plan can be found by its category", async done => {
-    const categoryRun = Category.RUN;
+describe("Unit testing", () => {
+  describe("PlanFinder", () => {
+    test("A plan can be found by its category", async done => {
+      const categoryRun = Category.RUN;
 
-    const planFinder = containerDI.resolve(PlanFinder);
-    const runPlan = await planFinder.findByCategory(categoryRun);
+      const planFinder = containerDI.resolve(PlanFinder);
+      const runPlan = await planFinder.findByCategory(categoryRun);
 
-    expect(runPlan.length).toBeGreaterThan(0);
-    expect(runPlan[0].category).toBe(categoryRun);
+      expect(runPlan.length).toBeGreaterThan(0);
+      expect(runPlan[0].category).toBe(categoryRun);
 
-    done();
-  }, 240000);
+      done();
+    }, 240000);
+  });
 });
