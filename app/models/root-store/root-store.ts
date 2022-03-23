7a24747226b9d6dce78b8ca65432101d7e912fb0
Hessam <s.hesam.ghasemi@gmail.com>
2022-03-23 16:46:50 +0430

    Add some comments

--- app/models/root-store/root-store.ts
+++ app/models/root-store/root-store.ts
@@ -7,10 +7,19 @@ import { User, UserModel } from "../user/user";
  */
 // prettier-ignore
 export const RootStoreModel = types.model("RootStore").props({
+  // Store for current authenticated user ???
   userStore: types.maybe(UserModel),
+  
+  // Store for current user owned plans ???
   userPlansStore: types.optional(PlanStoreModel, {} as any),
+
+  // Store for listing all available plans for user which this user can join them
   searchPlansStore: types.optional(PlanStoreModel, {} as any),
+
+  // TODO:  Do we need a store for all attending plans??
 }).actions(self => ({
+  // TODO: why this action is here?  Can we move it to 'UserModel' like what has been
+  // done in 'PlanStoreModel'?
   setUser: (user: User) => {
     self.userStore = user;
   },
