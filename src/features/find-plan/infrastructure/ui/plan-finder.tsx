74dc1e7c0aaff6cb8d5fc7748d95a2ea8c9bcfdd
Hessam <s.hesam.ghasemi@gmail.com>
2022-04-26 00:14:46 +0430

    Make search plans screen cleaner

--- src/features/find-plan/infrastructure/ui/plan-finder.tsx
+++ src/features/find-plan/infrastructure/ui/plan-finder.tsx
@@ -1,11 +1,9 @@
 import { observer } from "mobx-react-lite";
 import React, { PropsWithChildren, useEffect } from "react";
-import { ScrollView, StyleSheet, Text, TextStyle, ViewStyle } from "react-native";
-import { Button, FormRow } from "../../../../../app/components";
+import { Pressable, ScrollView, StyleSheet, Text, TextStyle, ViewStyle } from "react-native";
 import { useStores } from "../../../../../app/models";
 import { PlanSnapshot } from "../../../../../app/models/plan/plan";
 import { color, spacing, typography } from "../../../../../app/theme";
-import { palette } from "../../../../../app/theme/palette";
 import { Category } from "../../../../core/domain/plan";
 import { containerDI } from "../../../../core/infrastructure/dependency-injection/container";
 import { PlanFinder } from "../../application/plan-finder";
@@ -70,62 +68,69 @@ const PlanList: React.FC<PlanListProps> = observer((props: PlanListProps) => {
   //   searchPlansStore.savePlans(plans as PlanSnapshot[]);
   // };
 
-  const FilterButtons = ({ areButtonsShown }) => {
-    console.debug(areButtonsShown);
-    // if (props.owner) {
-    //   findRunPlansByOwner();
-    //   return <></>;
-    // }
+  // const FilterButtons = ({ areButtonsShown }) => {
+  //   console.debug(areButtonsShown);
+  //   // if (props.owner) {
+  //   //   findRunPlansByOwner();
+  //   //   return <></>;
+  //   // }
 
-    if (areButtonsShown) {
-      return (
-        <>
-          <FormRow preset={"clear"}>
-            <Button
-              textStyle={{ color: palette.black, fontSize: 18 }}
-              text="Walk Plans"
-              onPress={findWalkPlans}
-            />
-          </FormRow>
-          <FormRow preset={"clear"}>
-            <Button
-              textStyle={{ color: palette.black, fontSize: 18 }}
-              text="Run Plans"
-              onPress={findRunPlans}
-            />
-          </FormRow>
-        </>
-      );
-    }
+  //   if (areButtonsShown) {
+  //     return (
+  //       <>
+  //         <FormRow preset={"clear"}>
+  //           <Button
+  //             textStyle={{ color: palette.black, fontSize: 18 }}
+  //             text="Walk Plans"
+  //             onPress={findWalkPlans}
+  //           />
+  //         </FormRow>
+  //         <FormRow preset={"clear"}>
+  //           <Button
+  //             textStyle={{ color: palette.black, fontSize: 18 }}
+  //             text="Run Plans"
+  //             onPress={findRunPlans}
+  //           />
+  //         </FormRow>
+  //       </>
+  //     );
+  //   }
 
-    return <></>;
-  };
+  //   return <></>;
+  // };
 
   return (
     <>
       {/* TODO: revise this component */}
-      <FilterButtons areButtonsShown={true} />
+      {/* <FilterButtons areButtonsShown={true} /> */}
       <ScrollView>
         {searchPlansStore.plans.map(plan => {
           return (
-            <Section title={plan.title} key={plan.id}>
-              The plan is a <Text style={styles.highlight}>{plan.category}</Text> at{" "}
-              <Text style={styles.highlight}>{plan.location}</Text> around{" "}
-              <Text style={styles.highlight}>{new Date(plan.time).toLocaleDateString()}</Text>.
-              <Button
-                testID="next-screen-button-5"
-                style={CONTINUE}
-                textStyle={CONTINUE_TEXT}
-                text="MORE"
-                // ref: https://reactnavigation.org/docs/params/
-                onPress={() =>
-                  props.navigation.navigate({
-                    name: "joinPlanRequest",
-                    params: { planId: plan.id },
-                  })
-                }
-              />
-            </Section>
+            <Pressable onPress={() =>
+              props.navigation.navigate({
+                name: "joinPlanRequest",
+                params: { planId: plan.id },
+              })}>
+
+              <Section title={plan.title} key={plan.id} style={styles.planCard}>
+                The plan is a <Text style={styles.highlight}>{plan.category}</Text> at{" "}
+                <Text style={styles.highlight}>{plan.location}</Text> around{" "}
+                <Text style={styles.highlight}>{new Date(plan.time).toLocaleDateString()}</Text>.
+                {/* <Button
+                  testID="next-screen-button-5"
+                  style={CONTINUE}
+                  textStyle={CONTINUE_TEXT}
+                  text="MORE"
+                  // ref: https://reactnavigation.org/docs/params/
+                  onPress={() =>
+                    props.navigation.navigate({
+                      name: "joinPlanRequest",
+                      params: { planId: plan.id },
+                    })
+                  }
+                /> */}
+              </Section>
+            </Pressable>
           );
         })}
       </ScrollView>
@@ -141,4 +146,9 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: "700",
   },
+  planCard: {
+    backgroundColor: color.primaryDarker,
+    borderRadius: 6,
+    padding: 5
+  }
 });
